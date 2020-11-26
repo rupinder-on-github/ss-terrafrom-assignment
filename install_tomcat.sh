2b18c6c522c6e5f9aa32fba7e992d350ab419479
rupinder-on-github <46348882+rupinder-on-github@users.noreply.github.com>
2020-11-26 12:06:10 +0530

    Update install_tomcat.sh
    Html changes

--- install_tomcat.sh
+++ install_tomcat.sh
@@ -5,7 +5,7 @@ sudo systemctl start apache2
 cat >> /var/www/html/index.html << EOF
 <html>
 <head>
-  <title> Ubuntu rocks! </title>
+  <title> Test Assignment</title>
 </head>
 <body>
   <p> Site is running on tomcat host which is running in private subnet!
